7397207d4a5a80593abaf3ebc9fdf44fbd1ae538
Justice  Otuya <justice.otuya@digitalpulse.ng>
2019-08-17 01:19:03 +0100

    Updates

---
+++ precache-manifest.6bc0d2f6ab62508cb5960f1ca7be0c3d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5034083df979849bacba7a4fb97b733",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d04366dd506ff39592a6",
+    "url": "/static/css/main.02d4ee83.chunk.css"
+  },
+  {
+    "revision": "29b38933ba156bd16e61",
+    "url": "/static/js/2.99c254d8.chunk.js"
+  },
+  {
+    "revision": "d04366dd506ff39592a6",
+    "url": "/static/js/main.65a18e21.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "11ba15d111626d821197b8be146ada2f",
+    "url": "/static/media/character-1.11ba15d1.jpg"
+  },
+  {
+    "revision": "4019748034599ce71b7d94267985153e",
+    "url": "/static/media/character-2.40197480.jpg"
+  },
+  {
+    "revision": "1c623f72b1344d3c32d4dff4bf26ebac",
+    "url": "/static/media/character-3.1c623f72.jpg"
+  },
+  {
+    "revision": "07df505ab76825d7af809556a7a52019",
+    "url": "/static/media/character-4.07df505a.jpg"
+  },
+  {
+    "revision": "dd4c8bebf90b82f0fb4d072e97d9c9d8",
+    "url": "/static/media/hero-banner.dd4c8beb.jpg"
+  },
+  {
+    "revision": "153ffe7833e8167477c1b35576b8b210",
+    "url": "/static/media/next.153ffe78.svg"
+  },
+  {
+    "revision": "08506399a9f5c2279314aa8c9814bda5",
+    "url": "/static/media/planet-1.08506399.jpg"
+  },
+  {
+    "revision": "43fb38009ba82eafcbf4d62f525be54a",
+    "url": "/static/media/planet-2.43fb3800.jpg"
+  },
+  {
+    "revision": "4a7a31f65d6593b243065a181199f7b8",
+    "url": "/static/media/planet-3.4a7a31f6.jpg"
+  },
+  {
+    "revision": "278d8d8468abf147dbb87bba5b70f757",
+    "url": "/static/media/search.278d8d84.svg"
+  },
+  {
+    "revision": "a977d6d20bd1f8d7a351e781e2d5448e",
+    "url": "/static/media/starship-1.a977d6d2.jpg"
+  },
+  {
+    "revision": "4df52cf60042640a0ea102e65fda59e7",
+    "url": "/static/media/starship-2.4df52cf6.jpg"
+  },
+  {
+    "revision": "21bbc6abd95acb777b1242c37f0c9ed2",
+    "url": "/static/media/starship-3.21bbc6ab.jpg"
+  },
+  {
+    "revision": "92b2c815b6f740b0416fd4c36b957dc3",
+    "url": "/static/media/starship-4.92b2c815.jpg"
+  },
+  {
+    "revision": "70c4bcba1d2da096833d8f68b78366bc",
+    "url": "/static/media/starship-5.70c4bcba.jpg"
+  },
+  {
+    "revision": "340c3a0108cfc48c5e9c8e9a512f124e",
+    "url": "/static/media/starship-6.340c3a01.jpg"
+  }
+]);
